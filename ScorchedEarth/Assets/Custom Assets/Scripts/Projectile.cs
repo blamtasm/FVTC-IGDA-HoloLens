79d320975688234b84cb64fbb4e1572e0125c5c6
Tyler Terrell <Tyler Terrell>
2017-04-05 17:44:00 -0500

    Set up some basic objects

---
+++ ScorchedEarth/Assets/Custom Assets/Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile : MonoBehaviour {
+
+    public float DamageMinimum { get; set; }
+    public float DamageMaximum { get; set; }
+    public float Speed { get; set; }
+    public float MaxDistance { get; set; }
+    public double MaxFlightTime { get; set; }
+    public float ExplosionRadius { get; set; }
+    public GameObject ExplosionObject { get; set; }
+    public ParticleSystem ExplosionParticles { get; set; }
+    public bool SplitsProjectileOnCollision { get; set; }
+    public GameObject SplitProjectile { get; set; }
+    public AudioClip FireSoundEffect { get; set; }
+    public AudioClip ExplosionSoundEffect { get; set; }
+    public AudioClip FlightSoundEffect { get; set; }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public abstract void Detonate();
+
+    public abstract void OnCollisionEnter(Collider c);
+}
